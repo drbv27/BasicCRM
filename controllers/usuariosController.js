bc3e853028c335f98928966b9af6e91f9dbe5cc9
Diego <drbv27@gmail.com>
2021-12-19 08:14:21 -0500

    Endpoint and controller for new users

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,3 +1,18 @@
-exports.registrarUsuario = () => {};
+const Usuarios = require("../models/Usuarios");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.registrarUsuario = async (req, res) => {
+  //leer los datos del usuario y colocarlos en el modelo(Usuarios)
+  const usuario = new Usuarios(req.body);
+  usuario.password = await bcrypt.hash(req.body.password, 12);
+  try {
+    await usuario.save();
+    res.json({ mensaje: "Usuario creado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.json({ mensaje: "Hubo un error" });
+  }
+};
 
 exports.autenticarUsuario = () => {};
