d9aa9415eb55e2f491d391e9866adf802aa3d3ae
Diego <drbv27@gmail.com>
2021-12-24 08:42:48 -0500

    Habilitando CORS para recibir request de un dominio especifico

--- routes/index.js
+++ routes/index.js
@@ -17,55 +17,73 @@ module.exports = function () {
   });
 
   //Agrega nuevos clientes via POST
-  router.post("/clientes", clienteController.nuevoCliente);
+  router.post("/clientes", auth, clienteController.nuevoCliente);
 
   //Obtener todos los clientes
   router.get("/clientes", auth, clienteController.mostrarClientes);
   //Muestra un cliente en especifico(ID)
-  router.get("/clientes/:idCliente", clienteController.mostrarCliente);
+  router.get("/clientes/:idCliente", auth, clienteController.mostrarCliente);
   //Actualizar un cliente
-  router.put("/clientes/:idCliente", clienteController.actualizarCliente);
+  router.put("/clientes/:idCliente", auth, clienteController.actualizarCliente);
   //Eliminar Cliente
-  router.delete("/clientes/:idCliente", clienteController.eliminarCliente);
+  router.delete(
+    "/clientes/:idCliente",
+    auth,
+    clienteController.eliminarCliente
+  );
 
   /*-------PRODUCTOS-------*/
   //Nuevos productos
   router.post(
     "/productos",
+    auth,
     productosController.subirArchivo,
     productosController.nuevoProducto
   );
   //Muestra todos los productos
-  router.get("/productos", productosController.mostrarProductos);
+  router.get("/productos", auth, productosController.mostrarProductos);
   //Muestra un producto
-  router.get("/productos/:idProducto", productosController.mostrarProducto);
+  router.get(
+    "/productos/:idProducto",
+    auth,
+    productosController.mostrarProducto
+  );
   //Actualizar Producto
   router.put(
     "/productos/:idProducto",
+    auth,
     productosController.subirArchivo,
     productosController.actualizarProducto
   );
   //Eliminar Productos
-  router.delete("/productos/:idProducto", productosController.eliminarProducto);
+  router.delete(
+    "/productos/:idProducto",
+    auth,
+    productosController.eliminarProducto
+  );
   //Busqueda de Productos
-  router.post("/productos/busqueda/:query", productosController.buscarProducto);
+  router.post(
+    "/productos/busqueda/:query",
+    auth,
+    productosController.buscarProducto
+  );
 
   /*-------PEDIDOS-------*/
   //Agrega nuevos pedidos
-  router.post("/pedidos/nuevo/:idUsuario", pedidosController.nuevoPedido);
+  router.post("/pedidos/nuevo/:idUsuario", auth, pedidosController.nuevoPedido);
   //Mostrar pedidos
-  router.get("/pedidos", pedidosController.mostrarPedidos);
+  router.get("/pedidos", auth, pedidosController.mostrarPedidos);
   //Mostrar un pedido por ID
-  router.get("/pedidos/:idPedido", pedidosController.mostrarPedido);
+  router.get("/pedidos/:idPedido", auth, pedidosController.mostrarPedido);
   //Actualizar pedidos
-  router.put("/pedidos/:idPedido", pedidosController.actualizarPedido);
+  router.put("/pedidos/:idPedido", auth, pedidosController.actualizarPedido);
   //Eliminar un pedido por su Id
-  router.delete("/pedidos/:idPedido", pedidosController.eliminarPedido);
+  router.delete("/pedidos/:idPedido", auth, pedidosController.eliminarPedido);
   /*-----------------*/
 
   /*-------Usuarios------- */
   //Crear la cuenta
-  router.post("/crear-cuenta", usuariosController.registrarUsuario);
+  router.post("/crear-cuenta", auth, usuariosController.registrarUsuario);
   //Iniciar Sesión
   router.post("/iniciar-sesion", usuariosController.autenticarUsuario);
 
